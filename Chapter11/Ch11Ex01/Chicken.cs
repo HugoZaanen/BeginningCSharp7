7f8d8e8c92300db02a4ea5dfe997dd93c194ff67
Huego <hugozaanen@hotmail.com>
2018-09-20 10:15:59 +0200

    oefening boek chapter 11

---
+++ Chapter11/Ch11Ex01/Chicken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11Ex01
+{
+    class Chicken : Animal
+    {
+        public void Milk() => Console.WriteLine($"{name} has been milked.");
+        public Chicken(string newName) : base(newName) { }
+    }
+}
